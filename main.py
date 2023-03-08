a32d3826aa72df1b2f8faddb89ed52ee74f9283c
prolegosi <propastogorie@yandex.ru>
2023-03-08 12:59:31 +0300

    add connect

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import requests
+import json
 
+url = 'https://api.hh.ru/vacancies'
+job = ["'python' and 'junior'"]
+params = {'text': job, 'area': '113', 'per_page': '10'}
+r = requests.get(url, params=params)
+d = json.loads(r.text)
 
-
+for i in d.items():
+    print(i)
