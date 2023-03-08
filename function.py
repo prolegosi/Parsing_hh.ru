b774462211dc26ae42a392f80e2a9ed9b226739e
prolegosi <propastogorie@yandex.ru>
2023-03-08 16:15:00 +0300

    add pandas

--- function.py
+++ function.py
@@ -1,25 +1,36 @@
 import requests
 import json
+import pandas as pd
+def list_from_api(url, par=None):
+    r = requests.get(url, params=par)
+    out = json.loads(r.text)
+    if type(out) == dict:
+        out = out['items']
+    return out
 
 
 url_api = 'https://api.hh.ru/vacancies'
-url_country = "https://api.hh.ru/areas/"
+url_area = "https://api.hh.ru/areas/"
 
 job = ["'python' and 'стажёр'"]
 
 
 params = {'text': job, 'area': '113', 'per_page': '10'}
 
-
-def list_from_api(url, par=None):
-    r = requests.get(url, params=par)
-    out = json.loads(r.text)
-    return out
+# получение списка данных из API
 
 
-#d = jobs_list(url_api, params)
-d = list_from_api(url_country)
+
+c = list_from_api(url_api, params)
+d = list_from_api(url_area)
+
 for i in d:
     for j in i.items():
         print(j)
     print()
+
+for i in c:
+    for j in i.items():
+        print(j)
+    print()
+
