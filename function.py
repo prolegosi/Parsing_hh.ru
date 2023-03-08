a683d8647e3a1c47c7451b97f9a932a949069f27
prolegosi <propastogorie@yandex.ru>
2023-03-08 15:22:19 +0300

    Job_list => list_from_api

--- function.py
+++ function.py
@@ -1,13 +1,25 @@
 import requests
 import json
-#"url": "https://api.hh.ru/areas/
-url = 'https://api.hh.ru/vacancies'
-job = ["'python' and 'стажёр'"]
-params = {'text': job, 'area': '113', 'per_page': '10'}
-r = requests.get(url, params=params)
-d = json.loads(r.text)
 
-for i in d['items']:
+
+url_api = 'https://api.hh.ru/vacancies'
+url_country = "https://api.hh.ru/areas/"
+
+job = ["'python' and 'стажёр'"]
+
+
+params = {'text': job, 'area': '113', 'per_page': '10'}
+
+
+def list_from_api(url, par=None):
+    r = requests.get(url, params=par)
+    out = json.loads(r.text)
+    return out
+
+
+#d = jobs_list(url_api, params)
+d = list_from_api(url_country)
+for i in d:
     for j in i.items():
         print(j)
     print()
