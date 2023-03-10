bb458c0f008b77233a634afea716a9b09e7b690c
prolegosi <propastogorie@yandex.ru>
2023-03-10 20:45:29 +0300

    daa start set ui

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
 class MainApp(App):
     def build(self):
-        button = Button(text='Hello from Kivy',
-                        size_hint=(.5, .5),
-                        pos_hint={'center_x': .5, 'center_y': .5})
-        button.bind(on_press=self.on_press_button)
+        main_layout = BoxLayout(orientation="vertical")
 
-        return button
-
-    def on_press_button(self, instance):
-        print('Вы нажали на кнопку!')
+        self.label = Label(text='Hello from Kivy',
+                      size_hint=(.5, .5),
+                      pos_hint={'center_x': .5, 'center_y': .5})
 
 
-if __name__ == '__main__':
+        self.input = TextInput(
+            multiline=False, readonly=True, halign="right", font_size=55
+        )
+        self.button = Button(
+                text= 'button',
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+        )
+        main_layout.add_widget(self.input)
+        main_layout.add_widget(self.label)
+        main_layout.add_widget(self.button)
+
+        return main_layout
+
+if __name__ == "__main__":
     app = MainApp()
     app.run()
