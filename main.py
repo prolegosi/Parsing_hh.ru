5349a856415a447c079dd9f78d8f112c105b182b
prolegosi <propastogorie@yandex.ru>
2023-03-11 19:57:28 +0300

    add first page

--- main.py
+++ main.py
@@ -3,29 +3,77 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+
+
+Window.clearcolor = (0.16, 0.17, 0.2)
 
 
 class MainApp(App):
+    def click(self, event):
+        a = self.input_1.text
+        b = self.input_2.text
+        print(a, b)
     def build(self):
-        main_layout = BoxLayout(orientation="vertical")
-
-        self.label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
-
-
-        self.input = TextInput(
-            multiline=False, readonly=True, halign="right", font_size=55
+        main_layout = BoxLayout(orientation="vertical",
+                                padding=15,
+                                spacing=15
         )
+
+        self.label_1 = Label(text='Введите название города или региона',
+                             size_hint=(1, 1),
+                             pos_hint={'center_x': .5, 'center_y': .5},
+                             outline_color=[0, 0, 0],
+                             outline_width=0.5,
+                             )
+
+        self.input_1 = TextInput(
+            size_hint=(1, 1),
+            multiline=False,
+            halign="right",
+            font_size=14,
+        )
+        self.label_2 = Label(text='Введите названия профессий,\n    должностей через пробел',
+                             size_hint=(1, 1),
+                             pos_hint={'center_x': .5, 'center_y': .5},
+                             outline_color=[0, 0, 0],
+                             outline_width=0.5
+                             )
+
+        self.input_2 = TextInput(
+            size_hint=(1, 1),
+            multiline=False,
+            halign="right",
+            font_size=14
+        )
+        self.label_3 = Label(text='',
+                             size_hint=(1, 1),
+                             pos_hint={'center_x': .5, 'center_y': .5}
+                             )
         self.button = Button(
-                text= 'button',
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            size_hint=(.5, 1),
+            text='Поиск',
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+            background_normal='',
+            background_color=[0.18, 0.18, 0.2],
+            on_press=self.click
         )
-        main_layout.add_widget(self.input)
-        main_layout.add_widget(self.label)
+        self.label_4 = Label(text='',
+                             size_hint=(1, 1),
+                             pos_hint={'center_x': .5, 'center_y': .5}
+                             )
+
+        main_layout.add_widget(self.label_1)
+        main_layout.add_widget(self.input_1)
+        main_layout.add_widget(self.label_2)
+        main_layout.add_widget(self.input_2)
+        main_layout.add_widget(self.label_3)
         main_layout.add_widget(self.button)
+        main_layout.add_widget(self.label_4)
+
 
         return main_layout
+
 
 if __name__ == "__main__":
     app = MainApp()
