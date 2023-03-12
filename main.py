e590e9986b379ffb183f0455c43a6f81d5351130
prolegosi <propastogorie@yandex.ru>
2023-03-12 10:56:47 +0300

    add two page

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Window.clearcolor = (0.16, 0.17, 0.2)
 
 
-class MainApp(App):
-    def click(self, event):
-        a = self.input_1.text
-        b = self.input_2.text
-        print(a, b)
-    def build(self):
+class FirstPage(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         main_layout = BoxLayout(orientation="vertical",
                                 padding=15,
                                 spacing=15
-        )
+                                )
 
         self.label_1 = Label(text='Введите название города или региона',
                              size_hint=(1, 1),
@@ -33,6 +30,7 @@ class MainApp(App):
             halign="right",
             font_size=14,
         )
+
         self.label_2 = Label(text='Введите названия профессий,\n    должностей через пробел',
                              size_hint=(1, 1),
                              pos_hint={'center_x': .5, 'center_y': .5},
@@ -46,10 +44,12 @@ class MainApp(App):
             halign="right",
             font_size=14
         )
+
         self.label_3 = Label(text='',
                              size_hint=(1, 1),
                              pos_hint={'center_x': .5, 'center_y': .5}
                              )
+
         self.button = Button(
             size_hint=(.5, 1),
             text='Поиск',
@@ -58,6 +58,7 @@ class MainApp(App):
             background_color=[0.18, 0.18, 0.2],
             on_press=self.click
         )
+
         self.label_4 = Label(text='',
                              size_hint=(1, 1),
                              pos_hint={'center_x': .5, 'center_y': .5}
@@ -70,11 +71,30 @@ class MainApp(App):
         main_layout.add_widget(self.label_3)
         main_layout.add_widget(self.button)
         main_layout.add_widget(self.label_4)
+        self.add_widget(main_layout)
+
+    def click(self, event):
+        a = self.input_1.text
+        b = self.input_2.text
+        set_screen('Two_page')
+        print(type(a), len(b))
 
 
-        return main_layout
+class TwoPage(Screen):
+    def __int__(self):
+
+
+def set_screen(name_screen):
+    sm.current = name_screen
+
+sm = ScreenManager()
+sm.add_widget(FirstPage(name='First_page'))
+class JobsSkilApp(App):
+    def build(self):
+
+        return sm
 
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = JobsSkilApp()
     app.run()
